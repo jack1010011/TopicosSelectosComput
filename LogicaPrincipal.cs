5488849c4fe8950a0954f20e8faf7d549b821cbe
Shalom P <jack1010011@outlook.com>
2021-09-30 12:20:40 -0600

    Add project files.

---
+++ LogicaPrincipal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topicos.NetCore.Mensajeria.BL;
+
+namespace Topicos.NetCore.ConsoleApp
+{
+    public class LogicaPrincipal
+    {
+        public void HagaLaMagiaOld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void HagaLaMagia()
+        {
+            var elMensaje = new Mensaje("Yo","Usted","Hello world!");
+            var elServicio = new MensajeriaPlana();
+            elServicio.Enviar(elMensaje);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Topicos.NetCore.ConsoleApp;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+        {
+            HagaLaMagia();
+        }
+
+        private static void HagaLaMagia()
+        {
+            var laMagia = new LogicaPrincipal();
+            laMagia.HagaLaMagia();
+        }
+    }
+}
